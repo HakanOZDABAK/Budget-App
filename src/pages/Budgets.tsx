bd9b816d017f445b28724ae92b4290adb6f40f80
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-01-12 12:48:47 +0300

    Budget page added table

--- src/pages/Budgets.tsx
+++ src/pages/Budgets.tsx
@@ -5,84 +5,87 @@ import { Column } from 'primereact/column';
 import { InputText } from 'primereact/inputtext';
 import { SelectButton } from 'primereact/selectbutton';
 import { TreeNode } from 'primereact/treenode';
-
+import { DataTable } from 'primereact/datatable';
+import { FilterMatchMode } from 'primereact/api';
 interface FilterModeOption {
     label: string;
     value: string;
 }
 
-const myTreeNode: TreeNode = {
-  id: "unique_id",
-  key: "unique_key",
-  label: "Ali",
-  data: {
-    size: "100kb",
-    type: "Folder"
-  },
-  icon: null,
-  children: [
-    {
-      id: "sub_unique_id",
-      key: "sub_unique_key",
-      label: "Subfolder",
-      data: {
-        size: "50kb",
-        type: "Subfolder"
-      },
-      icon: null,
-      children: [], // Eğer alt düğüm eklemek istiyorsanız buraya ekleyebilirsiniz
-      style: {},
-      className: "optional-class",
-      droppable: true,
-      draggable: true,
-      selectable: true,
-      leaf: false,
-      expanded: false
-    }
-  ],
-  style: {},
-  className: "optional-class",
-  droppable: true,
-  draggable: true,
-  selectable: true,
-  leaf: false,
-  expanded: false
-};
+
 
 
 export default function Budgets() {
-  const [nodes, setNodes] = useState<TreeNode[]>([myTreeNode]);
-  const [globalFilter, setGlobalFilter] = useState<string>('');
-  const [filterMode, setFilterMode] = useState('lenient');
-  const [filterOptions] = useState<FilterModeOption[]>([
-      { label: 'Lenient', value: 'lenient' },
-      { label: 'Strict', value: 'strict' }
-  ]);
+const [nodes, setNodes] = useState<any>([{
+  id: 1000,
+  name: 'James Butt',
+  country: {
+      name: 'Algeria',
+      code: 'dz'
+  },
+  company: 'Benton, John B Jr',
+  date: '2015-09-13',
+  status: 'unqualified',
+  verified: true,
+  activity: 17,
+  representative: {
+      name: 'Ioni Bowcher',
+      image: 'ionibowcher.png'
+  },
+  balance: 70663
+},{
+  id: 1001,
+  name: 'Anes Butt',
+  country: {
+      name: 'Turkey',
+      code: 'dz'
+  },
+  company: 'Benton, John B Jr',
+  date: '2015-09-13',
+  status: 'unqualified',
+  verified: true,
+  activity: 17,
+  representative: {
+      name: 'Ioni Bowcher',
+      image: 'ionibowcher.png'
+  },
+  balance: 70663
+}]);
+const [filters, setFilters] = useState({
+  global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+});    const [globalFilterValue, setGlobalFilterValue] = useState('');
   console.log(nodes)
 
-  const getHeader = () => {
-      return (
-          <div className="flex justify-content-end">
-              <div className="p-input-icon-left">
-                  <i className="pi pi-search"></i>
-                  <InputText type="search" onInput={(e) => setGlobalFilter((e.target as HTMLInputElement).value)} placeholder="Global Search" />
-              </div>
-          </div>
-      );
-  };
 
-  let header = getHeader();
+  const onGlobalFilterChange = (e:any) => {
+    const value = e.target.value;
+    let _filters = { ...filters };
+
+    _filters['global'].value = value;
+
+    setFilters(_filters);
+    setGlobalFilterValue(value);
+};
+const renderHeader = () => {
+  return (
+      <div className="flex justify-content-end">
+          <span className="p-input-icon-left">
+              <i className="pi pi-search" />
+              <InputText value={globalFilterValue} onChange={onGlobalFilterChange} placeholder="Keyword Search" />
+          </span>
+      </div>
+  );
+};
+const header = renderHeader();
 
   return (
-      <div className="card mt-5">
-          <div className="flex justify-content-center mb-4">
-              <SelectButton value={filterMode} onChange={(e) => setFilterMode(e.value)} options={filterOptions} />
-          </div>
-          <TreeTable value={nodes} globalFilter={globalFilter} header={header} filterMode={filterMode as "lenient" | "strict" | undefined} tableStyle={{ minWidth: '50rem' }}>
-      <Column field="label" header="Name" expander filter filterPlaceholder="Filter by name"></Column>
-      <Column field="data.size" header="Size" filter filterPlaceholder="Filter by size"></Column>
-      <Column field="data.type" header="Type" filter filterPlaceholder="Filter by type"></Column>
-    </TreeTable>
-      </div>
+    <div className="card mt-5">
+    <DataTable value={nodes} scrollable scrollHeight="400px" style={{ minWidth: '50rem' }} globalFilterFields={['name', 'country.name', 'representative.name', 'status']} header={header} emptyMessage="No customers found.">
+        <Column field="name" filter  header="Name"></Column>
+        <Column field="country.name" filter  header="Country"></Column>
+        <Column field="representative.name" filter  header="Representative"></Column>
+        <Column field="company" filter  header="Company"></Column>
+    </DataTable>
+</div>
   )
 }
